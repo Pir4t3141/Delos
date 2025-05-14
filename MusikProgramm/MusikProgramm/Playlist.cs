5028a5954303b1edb7968936c18f465459c94838
Pir4t3141 <lamara.reich@gmail.com>
2025-05-14 15:32:34 +0200

    basic structure of classes

---
+++ MusikProgramm/MusikProgramm/Playlist.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusikProgramm
+{
+    public class Playlist
+    {
+        public int SongAnzahl { get; set; } //TODO: edit get; set;
+
+        public bool Shuffle { get; set; } = false;
+
+        public bool Repeat { get; set; } = false;
+
+        public string Name { get; set; }
+
+        public int Playtime { get; set; } //TODO: edit get; set;
+
+        public List<Song> SongList { get; set; }
+
+        public int currentSongs { get; set; } = 0;
+
+        public List<Song> SongListSorted { get; set; } // TODO: Make it so that it is songlist on start
+
+        public Playlist(string name)
+        {
+            //TODO  
+        }
+
+        public void import(string path)
+        {
+            //TODO: Import playlist
+        }
+
+        public void save(string path)
+        {
+            //TODO: save
+        }
+
+        public void addSong(Song song)
+        {
+            //TODO
+        }
+
+        public void removeSong(Song song)
+        {
+            //TODO
+        }
+
+        public void skip()
+        {
+            //TODO
+        }
+
+        public void sort()
+        {
+            //TODO: look how to do it
+        }
+
+        public void nextSong()
+        {
+            //TODO
+        }
+
+        public void previousSong()
+        {
+            //TODO
+        }
+
+        public void serialized()
+        {
+            //TODO
+        }
+    }
+}
---
+++ MusikProgramm/MusikProgramm/Song.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace MusikProgramm
+{
+    public class Song
+    {
+        public string Name { get; set; }
+
+        public int Lenght { get; private set; } // Playtime in seconds TODO: get from NAudio
+        public DateTime ReleaseDate { get; set; } // Date of release
+        public string Artist { get; set; } // Name of artist
+        public string Path { get; set; } // Path of file
+
+        public int Progress { get; set; } // How long you listened the last time TODO: save into here
+
+        public string Album { get; set; }
+
+        public Song(string path){
+            Path = path;
+        }
+        public void loadFromMetaData()
+        {
+            //TODO how to change
+        }
+
+        public void editMetaData()
+        {
+            //TODO
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} released on {ReleaseDate} by {Artist}";
+        }
+        
+        public string serializeToString()
+        {
+            //TODO
+            return $"TODO"; //TODO Correct Return
+        }
+
+        public static Song deserialize(string SerializedString)
+        {
+            return new Song("TODO"); //TODO: Correct Return
+        }
+    }
+}
